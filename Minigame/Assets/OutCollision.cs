3ba86de0dd0c8286845f53e50ffa9e3ee04aa2ef
Javier Gualda <javiercagn@gmail.com>
2019-04-09 14:20:49 +0200

    Added skybox and table

---
+++ Minigame/Assets/OutCollision.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutCollision : MonoBehaviour
+{
+
+    public GameObject collision;
+    public GameObject ball;
+    public GameObject ballspawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+           
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        void OnCollisionEnter(Collision col)
+        {
+            if(col.gameObject.name == "Bowling Ball")
+            {
+                Debug.Log("Colisión detectada");
+                Destroy(col.gameObject);
+
+            }
+        }
+    }
+}

075711555c68f5e90be7a337cd2e717f3e4782df
Javier Gualda <javiercagn@gmail.com>
2019-04-15 20:18:22 +0200

    Added shot tracker

---
+++ Minigame/Assets/ShotTracker.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotTracker : MonoBehaviour
+{
+    public GameObject trigger;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (trigger.GetComponent<BallCount>().counter == 0)
+            gameObject.GetComponent<TextMesh>().text = "Shot 1";
+        else if (trigger.GetComponent<BallCount>().counter == 1)
+            gameObject.GetComponent<TextMesh>().text = "Shot 2";
+    }
+}
